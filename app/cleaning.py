1590ef1c566311fd384e8a31c0edd8dd293891cd
galeka <75578995+galeka@users.noreply.github.com>
2024-11-05 16:45:48 +0700

    Update cleaning.py
    add exception to handle existing folders

--- app/cleaning.py
+++ app/cleaning.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 import time
+import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from locations import *
 
-
 class ShiftingFiles(FileSystemEventHandler):
     """
-    This class is going to allow us to override the FileSystemEventHandler
-    methods.
+    This class allows us to override the FileSystemEventHandler methods.
     """
     download_folders = ["photos", "documents", "folders", "music", "zipFiles",
                         "videos", "others", ".DS_Store", "code"]
-
-    # Overriding the on_modified() method of FileSystemEventHandler class
-    # in the watchdog API
 
     def on_modified(self, event):
         self.check_if_downloading()
@@ -23,36 +19,26 @@ class ShiftingFiles(FileSystemEventHandler):
             self.shift()
 
     def shift(self):
-        """
-        This function shifts all the files from one location to another,
-        excluding the folders that are created by create folder function.
-        :return:
-        """
+        """Shift all files from one location to another."""
         try:
             existing_files = os.listdir(download_location)
             for file_name in existing_files:
                 if file_name not in folders:
                     if file_name != ".DS_Store":
                         transfer_folder = self.which_location(file_name)
-                        os.rename(download_location +"/"+ file_name,
-                        transfer_folder +"/"+file_name)
+                        os.rename(download_location + "/" + file_name,
+                                  transfer_folder + "/" + file_name)
         except FileNotFoundError:
             print("Error Occurred: Some thing went wrong!")
             print("Please run the script again, if the script stops")
             exit()
 
     def which_location(self, file_name):
-        """
-        This function decides what location is suitable for a kind of file
-        :param file_name:
-        :return: location in string format
-        """
+        """Decide what location is suitable for a kind of file."""
         if file_name.find(".") != -1:
             temp = file_name.split(".")
-            file_ext = temp[len(temp) - 1]
-            file_ext = file_ext.lower()
-            if file_ext in ["jpg", "tiff", "gif", "png", "raw", "jpeg", "CR2",
-                            "HEIC"]:
+            file_ext = temp[-1].lower()
+            if file_ext in ["jpg", "tiff", "gif", "png", "raw", "jpeg", "CR2", "HEIC"]:
                 return photo_location
             if file_ext in ["mp3", "wav"]:
                 return music_location
@@ -71,15 +57,11 @@ class ShiftingFiles(FileSystemEventHandler):
             return other_location
 
     def check_if_downloading(self):
-        """
-        This function is used to check if there is any downloading at the
-        moment and then allows the script to shift the files.
-        :return:
-        """
+        """Check if there is any downloading at the moment."""
         new_files = os.listdir(download_location)
         for file_name in new_files:
             temp = file_name.split(".")
-            file_ext = temp[len(temp) - 1]
+            file_ext = temp[-1]
             if ".com.google.Chrome" not in file_name:
                 if "crdownload" in file_ext or "download" in file_ext:
                     time.sleep(60)
@@ -89,25 +71,19 @@ class ShiftingFiles(FileSystemEventHandler):
                 self.check_if_downloading()
         return None
 
-
 def create_folders():
+    """Create necessary folders in the download location."""
     for folder_name in folders:
-        existing_folders = os.listdir(download_location)
-        if folder_name not in existing_folders:
-            os.mkdir(os.path.join(download_location, folder_name))
-
+        folder_path = os.path.join(download_location, folder_name)
+        os.makedirs(folder_path, exist_ok=True)  # Creates the directory if it doesn't exist
 
 if __name__ == "__main__":
-    # The folders that need to be created
     folders = ["photos", "documents", "folders", "music", "zipFiles", "videos",
-               "others", "code"]
-    # To create separate folders to organize data in
-    create_folders()
+               "others", "code"]  # Folders to create
+    create_folders()  # Create necessary folders
+    ShiftingFiles().shift()  # Shift files as soon as the program is run
 
-    # To shift the files as soon as the program is run
-    ShiftingFiles().shift()
-
-    # Consuming watchdog API
+    # Set up the watchdog observer
     event_handler = ShiftingFiles()
     observer = Observer()
     observer.schedule(event_handler, download_location, recursive=False)
@@ -118,3 +94,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
+
